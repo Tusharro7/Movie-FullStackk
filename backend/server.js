df7e9156ca481b7c02bbccac89329774b55aa7d0
TUSHAR MAKAVANA <makavanatushar2345@gmail.com>
2026-05-11 15:18:21 +0530

    fix movies slice export mismatch

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,30 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import movieRoutes from "./routes/movieRoutes.js";
+  import express from "express";
+  import cors from "cors";
+  import dotenv from "dotenv";
 
-// dotenv.config();
+  dotenv.config();
 
-// const app = express();
+  import router from "./Routes/router.js";
 
-// app.use(cors());
-// app.use(express.json());
+  const app = express();
+  const PORT = process.env.PORT || 5000;
 
-// app.use("/", movieRoutes);
+  app.use(cors());
+  app.use(express.json());
 
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-import movieRoutes from "./routes/movieRoutes.js";
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend Running");
-});
-
-app.get("/test", (req, res) => {
-  res.json({
-    message: "Backend working",
-    tokenExists: Boolean(process.env.TMDB_TOKEN),
+  app.get("/", (req, res) => {
+    res.send("Backend Running");
   });
-});
 
-app.use("/", movieRoutes);
+  app.get("/test", (req, res) => {
+    res.json({
+      message: "Backend working",
+      tokenExists: Boolean(process.env.TMDB_TOKEN),
+    });
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.use("/", router);
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
